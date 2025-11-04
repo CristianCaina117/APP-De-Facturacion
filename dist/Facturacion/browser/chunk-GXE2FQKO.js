93f0308087c865c24f44d8bcde18cf68dd7e6fb8
CristianCaina117 <cristiandavidcaina1117@gmail.com>
2025-11-04 11:30:16 -0500

    Finalizacion del Software

---
+++ dist/Facturacion/browser/chunk-GXE2FQKO.js
@@ -0,0 +1,1 @@
+import{Ea as a,Za as s,_a as n,gb as o}from"./chunk-3LP2OGE5.js";var i=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=a({type:t,selectors:[["app-gas1y2"]],decls:2,vars:0,template:function(e,m){e&1&&(s(0,"p"),o(1,"gas1y2 works!"),n())},encapsulation:2})};export{i as Gas1y2};
